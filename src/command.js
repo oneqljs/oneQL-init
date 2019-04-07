4c8259e757342602b114436c2885d6e1fb362b68
skyweaver213 <happyday0213@126.com>
2019-04-07 18:00:20 +0800

    [update] oneql init

--- src/command.js
+++ src/command.js
@@ -1,32 +1,157 @@
-let packInfo = require('../package.json') || ''
+const packInfo = require('../package.json') || ''
+const yargs = require('yargs')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const urllib = require('urllib')
+const path = require('path')
+const homedir = require('node-homedir')
+let boilName = 'oneql-boilerplate-simple'
+const os = require('os')
+const compressing = require('compressing')
+const copydir = require('node-copydir')
+console.log(process.argv)
 
 module.exports = class command {
     constructor() {
-        const currentPath = process.cwd()
-        console.log('currentPath---', currentPath, ' package ', packInfo)
-        this.packageName = packInfo.name
-        this.currentPath = currentPath  
+        const cwd = process.cwd()
+        this.packageName = boilName // packInfo.name
+        this.cwd = cwd  
+        this.argv = yargs.argv
+        // 目录
+        this.dir = this.argv._[0]
+        // console.log('this. argv ', this.argv)
+        this.targetDir = path.resolve(cwd, this.dir)
+
+        this.request = urllib.request
+        this.httpClient = urllib.create();
 
     }
 
-    init() {
+    async curl(url, options) {
+        return await this.httpClient.request(url, options)
+    }
+
+    async init() {
+        const flag = await this.createCustromDir()
+        if (!flag) return
+
+        // todo 选择模板
+
+        // 下载模板包
+        let tempDir = await this.downloadBoilerplate(this.packageName)
+
+        // 第二个参数，只能给相对路径
+        await this.copyFiles(tempDir, this.dir)
+
+        this.printUsage()
+    }
+
+    // 使用信息
+    printUsage() {
+        this.log(`usage:
+          - cd ${this.targetDir}
+          - npm install
+          - npm start / npm run dev / npm test
+        `)
+    }
+
+    // 项目初始化自定义目录
+    async createCustromDir() {
+        let dir = this.dir
+        // 检查是否为空文件夹
+        let emptyDir = this.isEmptyDir(dir)
+
         // 初始化目录结构
+        if (emptyDir) {
+            // await this.createDir(dir)
+            return true
+        } else {
+            let files = fs.readdirSync(dir).filter(name => name[0] !== '.');
 
+            this.log(`${dir} already exists and not empty:` + JSON.stringify(files))
+            return false
+        }
+    }
 
+    log(str) {
+        process.stdout.write(str)
+    }
+
+    // isEmptyDir
+    isEmptyDir(dir) {
+        return !fs.existsSync(dir)
     }
 
     // 创建文件结构
-    createDirTree() {
-
+    async createDir(dir) {
+        await mkdirp.sync(dir);
     }
 
     // 获取package包信息
-    getPackageInfo() {
+    async getPackageInfo (pkg) {
+        let packUrl =  `${this.getRegistryByType()}/${pkg}/latest`
+
+        console.log('pack Url ', packUrl)
+
+        let packInfo = await this.request(packUrl)
+
+        let pack = packInfo.data.toString()
+
+        return JSON.parse(pack)
 
     }
 
     // 获取模板
-    downloadBoilerplate(boilName) {
+    async downloadBoilerplate(boilName) {
+        const packInfo = await this.getPackageInfo(boilName)
+        const tarball = packInfo.dist.tarball
+
+        const saveDir = path.join(os.tmpdir(), 'oneql-init-boilerplate')
+
+        // await rimraf(saveDir)
+
+        console.log('saveDir', saveDir)
+
+        this.log(`downloading ${tarball}`)
+        const res = await this.curl(tarball, { streaming: true, followRedirect: true })
+        await compressing.tgz.uncompress(res.res, saveDir)
+
+        this.log(`extract to ${saveDir}`)
+
+        return path.join(saveDir, '/package')
 
     }
+
+    // copy
+    async copyFiles(from, to) {
+        return await copydir(from, to)
+    }
+
+    getRegistryByType(_key) {
+
+        return 'https://registry.npm.taobao.org';
+
+        // switch (key) {
+        //   case 'china':
+        //     return 'https://registry.npm.taobao.org';
+        //   case 'npm':
+        //     return 'https://registry.npmjs.org';
+        //   default: {
+        //     if (/^https?:/.test(key)) {
+        //       return key.replace(/\/$/, '');
+        //     } else {
+        //       // support .npmrc
+        //       const home = homedir();
+        //       let url = process.env.npm_registry || process.env.npm_config_registry || 'https://registry.npmjs.org';
+        //       if (fs.existsSync(path.join(home, '.cnpmrc')) || fs.existsSync(path.join(home, '.tnpmrc'))) {
+        //         url = 'https://registry.npm.taobao.org';
+        //       }
+        //       url = url.replace(/\/$/, '');
+        //       return url;
+        //     }
+        //   }
+        // }
+
+      }
+
 }
